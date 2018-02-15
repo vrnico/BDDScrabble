8713a95f1d4b7563edae002d69a527d882283860
Michael Albers and Nico Daunt <michael@>
2018-02-15 11:15:03 -0800

    create green test condition for IndexReturnCorrectView Controller  test method

--- Scrabble.Tests/ControllerTests/HomeControllerTest.cs
+++ Scrabble.Tests/ControllerTests/HomeControllerTest.cs
@@ -1,13 +1,23 @@
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using Microsoft.AspNetCore.Mvc;
-// using Scrabble.Models;
-// using Scrabble.Controllers;
-//
-// namespace Scrabble.Tests
-// {
-//   [TestClass]
-//   public class HomeControllerTest
-//   {
-//
-//   }
-// }
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using Scrabble.Models;
+using Scrabble.Controllers;
+
+namespace Scrabble.Tests
+{
+  [TestClass]
+  public class HomeControllerTest
+  {
+     [TestMethod]
+     public void Index_ReturnCorrectView_true()
+     {
+         //arrange
+        HomeController controller = new HomeController();
+        //act
+        IActionResult indexView = controller.Index();
+        ViewResult result = indexView as ViewResult;
+        //assert
+        Assert.IsInstanceOfType(result, typeof(ViewResult));
+     } 
+  }
+}
--- Scrabble/Controllers/HomeController.cs
+++ Scrabble/Controllers/HomeController.cs
@@ -1,27 +1,26 @@
-// using Microsoft.AspNetCore.Mvc;
-// using Scrabble;
-//
-// namespace Scrabble.Controllers
-// {
-//   public class HomeController : Controller
-//   {
-//     // [HttpGet("/")]
-//     // public ActionResult Index()
-//     // {
-//     //   string emptyString = "";
-//     //   return View("Index", emptyString);
-//     // }
-//     //
-//     // [HttpPost("/result")]
-//     // public ActionResult Result()
-//     // {
-//     //   string player1 = Request.Form["player1-input"];
-//     //   string player2 = Request.Form["player2-input"];
-//     //
-//     //   Game newGame = new Game(player1, player2);
-//     //   newGame.SetWinner(player1, player2);
-//     //   string winner = newGame.GetWinner();
-//     //   return View("Index", winner);
-//     }
-//   }
-// }
+using Microsoft.AspNetCore.Mvc;
+using Scrabble.Models;
+
+namespace Scrabble.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      string emptyString = "";
+      return View("Index", emptyString);
+    }
+
+    [HttpPost("/result")]
+    public ActionResult Result()
+    {
+      string word = Request.Form["word-input"];
+
+      ScrabbleGenerator newScrabbleGenerator = new ScrabbleGenerator(word);
+      newScrabbleGenerator.BreakWord();
+      int wordVal = newScrabbleGenerator.WordCharVal();
+      return View("Index", wordVal);
+    }
+  }
+}
